1c8f6534761bd9a0e1668eb07ce26010284b8659
Jordan Fudge <jordanfudge95@gmail.com>
2023-06-30 20:55:23 -0400

    Removed Custom Bootstrap Styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,7 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/styles/bootstrap.custom.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
