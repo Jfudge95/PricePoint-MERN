375001352b122b925812152c29e33482f688e14a
Jordan Fudge <jordanfudge95@gmail.com>
2023-07-07 17:59:33 -0400

    Added User Routes and Controllers

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 connectDB(); // Connect MongoDB
 
 const app = express();
@@ -15,6 +16,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
