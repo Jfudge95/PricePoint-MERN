1128624c6c6ebd05b63a385b3f5d7828d77a9784
Jordan Fudge <jordanfudge95@gmail.com>
2023-07-15 12:01:48 -0400

    Added Delete User Functionality for Admins

--- frontend/src/pages/admin/UserList.jsx
+++ frontend/src/pages/admin/UserList.jsx
@@ -3,13 +3,27 @@ import { Table, Button } from "react-bootstrap";
 import { FaTrash, FaEdit, FaCheck, FaTimes } from "react-icons/fa";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
-import { useGetUsersQuery } from "../../slices/usersApiSlice";
+import {
+  useGetUsersQuery,
+  useDeleteUserMutation,
+} from "../../slices/usersApiSlice";
+import { toast } from "react-toastify";
 
 const UserList = () => {
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
 
+  const [deleteUser] = useDeleteUserMutation();
+
   const deleteHandler = async (id) => {
-    console.log("delete");
+    if (window.confirm("Are you sure")) {
+      try {
+        await deleteUser(id);
+        toast.success("User deleted");
+        refetch();
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
   };
 
   return (
