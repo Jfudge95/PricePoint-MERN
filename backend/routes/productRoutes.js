8e210893b768b5c670e69a523ab2d9f416d1fb58
Jordan Fudge <jordanfudge95@gmail.com>
2023-07-15 09:27:01 -0400

    Admin Create Product now fills with sample data

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,9 +5,11 @@ const router = Express.Router();
 import {
   getProducts,
   getProductById,
+  createProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductById);
 
 export default router;
